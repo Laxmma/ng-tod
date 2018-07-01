fd6b4776090e74fd071847a58a19e16632b345c4
Laxman Kumar <laxman.apr9@gmail.com>
2018-07-01 16:18:49 +0530

    eddited app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { TodoModule } from './todo/todo.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   RouterModule,
+ TodoModule,
+   AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
